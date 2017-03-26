d80239c48805e05bc0dae15ddd9552b63fbaed03
Martino Mensio <martinomensio@outlook.it>
2017-03-26 14:15:29 +0200

    web filter for /private/* URLs
    the web filter redirects the users to a login.jsp page (not yet existing) if the user is not authenticated

---
+++ src/main/java/it/polito/ai/businesslogic/UrlFilter.java
@@ -0,0 +1,45 @@
+package it.polito.ai.businesslogic;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/private/*")
+public class UrlFilter implements Filter {
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("filter running");
+		if (request instanceof HttpServletRequest && response instanceof HttpServletResponse) {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+			HttpSession session = httpRequest.getSession();
+			LoginService loginService = (LoginService) session.getAttribute("login");
+			if (loginService != null && loginService.isLoggedIn()) {
+				// pass the request along the filter chain
+				chain.doFilter(request, response);
+			} else {
+				httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.jsp");
+			}
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
