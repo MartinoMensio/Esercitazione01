2fa4015141aa409b4873f0d487587cd20adcc128
Riccardo Persiani <s225289@studenti.polito.it>
2017-03-29 18:20:04 +0200

    CartServlet DoPost()

--- src/main/java/it/polito/ai/servlets/CartServlet.java
+++ src/main/java/it/polito/ai/servlets/CartServlet.java
@@ -2,6 +2,7 @@ package it.polito.ai.servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,24 +12,34 @@ import it.polito.ai.businesslogic.CartService;
 /**
  * Servlet implementation class CartServlet
  */
+@WebServlet("/cart/actions")
 public class CartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		CartService cartService = (CartService) request.getSession().getAttribute("cartService");
-		
+		String actionType = request.getParameter("type");
 		String travelDocumentId = request.getParameter("travelDocumentId");
 		String quantity = request.getParameter("quantity");
-		
-		if(cartService != null){
-			//Success
+
+		if (actionType.equals("add")) {
+			System.out.println("Adding " + quantity+" "+ travelDocumentId);
+			// create a checkout from the cart
 			cartService.addItem(travelDocumentId, Integer.parseInt(quantity));
+			// send the client to the checkout page
+			response.sendRedirect(request.getContextPath() + "/cart.jsp");
+		}else if(actionType.equals("modify")){
+			System.out.println("Updating: " + quantity+" "+ travelDocumentId);
+			cartService.updateItem(travelDocumentId, Integer.parseInt(quantity));
+			// send the client to the checkout page
+			response.sendRedirect(request.getContextPath() + "/cart.jsp");
 		}else{
 			//Fail
+			System.out.println("CartServlet operation failed");
 			response.sendRedirect(request.getContextPath() + "/login.jsp");
 		}
 
